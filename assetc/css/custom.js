043fd554add5fa8f7870993174998e1a89a0d141
muhammad-sarif-islam <sharifulislam1971h@gmail.com>
2024-08-26 22:18:14 +0600

    sharif

---
+++ assetc/css/custom.js
@@ -0,0 +1,90 @@
+// let students = [
+//   {
+//     name: 'student name one',
+//     email: 'email one',
+//     phone: 'phone one',
+//   },
+//    {
+//     name: 'student name two',
+//     email: 'email two',
+//     phone: 'phone two',
+//   },
+//     {
+//     name: 'student name theree',
+//     email: 'email theree',
+//     phone: 'phone theree',
+//   }
+    
+// ]
+
+// // console.log(students[0]);
+// // console.log(students[1]);
+// // console.log(students[2]);
+
+
+
+// students.forEach(function (student) {
+//   console.log(students.name);
+// })
+
+
+
+
+
+// *********************// backup.js  *********************
+
+
+//   function Student(name, roll, className, section) {
+//     this.name = name;
+//     this.roll = roll;
+//     this.className = className;
+//     this.section = section;
+// }
+ 
+// let sharif = new Student('sharif',30, 'class 01', 'one');
+
+// console.log(sharif);
+
+// let myobj = {
+//   firstName: 'sharif',
+//    lastName: 'islam',
+//   roll: 12,
+//   className: 'class 5',
+//   section: 'two',
+//   fullName: function () {
+//     return this.firstName + ' ' + this.lastName;
+//   }
+// }
+
+// myobj.roll = 100;
+
+// console.log(myobj);
+
+
+
+          // arr
+
+let arr = [1, 2, 3, 4, 5, 6, 7,8,9,10];
+
+
+let output = arr[3];
+// output = arr.length;
+// output = arr.toString();
+// output = arr.join(' ');
+// output = arr.pop();
+// output = arr.push();
+// output = arr.shift();
+// output = arr.unshift('hello');
+// output = arr.splice(4, 2, 'one', 'two');
+//  output = arr.slice(arr.length - 7);
+output = arr.slice(-4);
+
+
+
+console.log(arr);
+console.log(output );
+
+
+
+
+        // arr
---
+++ assetc/js/backup.js
@@ -0,0 +1,38 @@
+//   function Student(name, roll, className, section) {
+//     this.name = name;
+//     this.roll = roll;
+//     this.className = className;
+//     this.section = section;
+// }
+ 
+// let sharif = new Student('sharif',30, 'class 01', 'one');
+
+// console.log(sharif);
+
+// let myobj = {
+//   firstName: 'sharif',
+//    lastName: 'islam',
+//   roll: 12,
+//   className: 'class 5',
+//   section: 'two',
+//   fullName: function () {
+//     return this.firstName + ' ' + this.lastName;
+//   }
+// }
+
+// myobj.roll = 100;
+
+// console.log(myobj);
+
+
+
+          // arr
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr);
+
+
+
+
+        // arr
